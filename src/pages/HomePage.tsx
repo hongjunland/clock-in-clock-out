5c6f789a931ea85b91bfcc0ea44f52677218f755
hongjunland <zunza96@gmail.com>
2023-03-23 22:22:41 +0900

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,59 @@
 import styled from "@emotion/styled";
 import logo from "../assets/logo.png";
 import { FaPlus } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
 
 export default function HomePage() {
   return (
     <Container>
       <main>
-        <LogoBox>
+        <LogoSection>
           <Logo src={logo} alt={"logo"} />
           <MainTitle>
             <span>Noris Work Dashboard</span>
           </MainTitle>
-        </LogoBox>
-        <ContentBoxGroup>
-          <ContentBox>
+        </LogoSection>
+        <ContentSection>
+          <ContentBoxGroup>
+            <ContentBox>
+              <ContentBoxHeader>
+                <ContentBoxTitle>연차 현황</ContentBoxTitle>
+                <ContentBoxButton>
+                  <FaPlus />
+                </ContentBoxButton>
+              </ContentBoxHeader>
+              <ContentBoxMain>
+                <ContentValue>15일</ContentValue>
+              </ContentBoxMain>
+            </ContentBox>
+            <ContentBox>
+              <ContentBoxHeader>
+                <ContentBoxTitle>오늘의 일정</ContentBoxTitle>
+                <ContentBoxButton>
+                  <FaPen />
+                </ContentBoxButton>
+              </ContentBoxHeader>
+              <ContentBoxMain>
+                <ContentTodoList>
+                  <li>오늘의 일정!</li>
+                </ContentTodoList>
+              </ContentBoxMain>
+            </ContentBox>
+            <ContentBox>
+              <ContentBoxHeader>
+                <ContentBoxTitle>출/퇴근</ContentBoxTitle>
+              </ContentBoxHeader>
+              <ContentBoxMain>
+                <ContentToggleButton>출근</ContentToggleButton>
+              </ContentBoxMain>
+            </ContentBox>
+          </ContentBoxGroup>
+          <ContentChartBox>
             <ContentBoxHeader>
-              <ContentBoxTitle>연차 현황</ContentBoxTitle>
-              <ContentBoxButton>
-                <FaPlus />
-              </ContentBoxButton>
+              <ContentBoxTitle>금주 근무시간</ContentBoxTitle>
             </ContentBoxHeader>
-            <ContentValue>15일</ContentValue>
-          </ContentBox>
-          <ContentBox>
-            <ContentBoxTitle>연차 현황</ContentBoxTitle>
-          </ContentBox>
-          <ContentBox>
-            <ContentBoxTitle>연차 현황</ContentBoxTitle>
-          </ContentBox>
-        </ContentBoxGroup>
+          </ContentChartBox>
+        </ContentSection>
       </main>
     </Container>
   );
@@ -39,12 +64,13 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  font-family: "HanSans";
 `;
 const Logo = styled.img`
   width: 89px;
   height: 89px;
 `;
-const LogoBox = styled.div`
+const LogoSection = styled.section`
   display: flex;
   flex-direction: row;
 `;
@@ -52,39 +78,77 @@ const MainTitle = styled.div`
   margin: auto 0;
   span {
     font-size: 21px;
-    font-family: "HanSans";
     vertical-align: middle;
   }
+`;
+const ContentSection = styled.section`
+  padding-left: 4rem;
+  padding-right: 4rem;
 `;
 const ContentBoxGroup = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 0 2rem 2rem 0;
 `;
 const ContentBox = styled.div`
-  font-family: "HanSans";
+  display: flex;
+  flex-direction: column;
+  aspect-ratio: 3/2;
+  width: 30%;
   font-size: 18px;
   border-radius: 10px;
   line-height: 3em;
   border-width: 2px;
   border-style: solid;
   border-color: rgb(224, 224, 224);
-  flex: 1;
-  margin: 2rem;
   color: #333;
 `;
 const ContentBoxHeader = styled.div`
   display: flex;
+  padding: 0 1rem;
+  height: 54px;
 `;
 const ContentBoxTitle = styled.div`
-  font-size: 16px;
-  font-weight: 700;
+  font-size: 18px;
+  font-weight: 800;
 `;
 const ContentBoxButton = styled.div`
   margin-left: auto;
   margin-right: 0;
+  color: rgb(224, 224, 224);
 `;
-const ContentValue = styled.span`
-  margin: auto;
+const ContentBoxMain = styled.div`
+  display: flex;
+  flex: 1;
+`;
+const ContentValue = styled.p`
+  margin: auto auto;
+  font-size: 2rem;
+`;
+const ContentTodoList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0 1rem 1rem 1rem;
   width: 100%;
+`;
+const ContentChartBox = styled.div`
+  border-radius: 10px;
+  background-color: rgb(255, 255, 255);
+  line-height: 3em;
+  border-width: 2px;
+  border-style: solid;
+  border-color: rgb(224, 224, 224);
+  aspect-ratio: 14/5;
+  margin-top: 2rem;
+`;
+const ContentToggleButton = styled.button`
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  margin: auto auto;
+  color: rgb(255, 255, 255);
+  text-align: center;
+  line-height: 2.5em;
+  border: 0;
+  border-radius: 4px;
+  background-color: rgb(52, 152, 219);
 `;
